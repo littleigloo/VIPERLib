919bde764032591d0c89c89ddd774e8e81d234e7
epitonium <epitonium@gmail.com>
2024-05-12 02:59:52 +0300

    NewRoot method made public

--- Sources/VIPERLib/Bases/Base.Router.swift
+++ Sources/VIPERLib/Bases/Base.Router.swift
@@ -130,34 +130,14 @@ open class Router: RouterInterface {
     public func stack(_ stackAction: UINavigationController.StackAction, with modules: [ModuleProtocol], style: ControllerPresentationStyle.PushedStyle = .default, isAnimated: Bool = false) {
         navigationController?.stack(stackAction, with: modules, style: style, isAnimated: isAnimated)
     }
-    
-    //  MARK: - WINDOW
-    public func newRoot(module: ModuleProtocol, isAnimated: Bool = true, completion: (() -> Void)? = nil) {
-        // Get main window
-        guard let window = UIApplication.shared.keyWindow else {
-            return
-        }
-        // Set the new rootViewController of the window
-        // Calling "UIView.transition" below will animate the swap
-        window.rootViewController = module.getController()
-        // Check if animation needed
-        guard isAnimated else {
-            completion?()
-            return
-        }
-        // A mask of options indicating how you want to perform the animations
-        let options: UIView.AnimationOptions = .transitionCrossDissolve
-        // The duration of the transition animation, measured in seconds
-        let duration: TimeInterval = 0.3
-        // Create a transition animation
-        UIView.transition(with: window, duration: duration, options: options, animations: {}) { _ in
-            completion?()
-        }
-    }
-    
     // Open url inside Safari
     public func openSafari(url: URL) {
         let safariViewController = SFSafariViewController(url: url)
         controller.present(safariViewController, animated: true)
+    }
+    
+    //  MARK: - WINDOW
+    public func newRoot(module: ModuleProtocol, isAnimated: Bool = true, completion: (() -> Void)? = nil) {
+        Utils.newRoot(module: module, isAnimated: isAnimated, completion: completion)
     }
 }
